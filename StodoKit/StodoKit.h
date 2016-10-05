626fc3196bd3797e4b2236d96272f0477bbc6377
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2016-10-06 07:34:27 +0900

    Setup for tool

---
+++ StodoKit/StodoKit.h
@@ -0,0 +1,19 @@
+//
+//  StodoKit.h
+//  StodoKit
+//
+//  Created by Hiroki Nagasawa on 10/6/16.
+//  Copyright © 2016 Hiroki Nagasawa. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StodoKit.
+FOUNDATION_EXPORT double StodoKitVersionNumber;
+
+//! Project version string for StodoKit.
+FOUNDATION_EXPORT const unsigned char StodoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StodoKit/PublicHeader.h>
+
+
---
+++ stodo/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  stodo
+//
+//  Created by Hiroki Nagasawa on 10/6/16.
+//  Copyright © 2016 Hiroki Nagasawa. All rights reserved.
+//
+
+import Foundation
+
+print("Hello")
