cd58f6429c84a959d8bff855f3da15fcb5cf8244
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2016-12-10 11:34:17 +0900

    Add .Package.test.swift

---
+++ .Package.test.swift
@@ -0,0 +1,16 @@
+import PackageDescription
+
+let package = Package(
+    name: "stodo",
+    targets: [
+        Target(name: "StodoKit"),
+        Target(name: "stodo",
+              dependencies: [.Target(name: "StodoKit")]),
+    ],
+    dependencies: [
+		.Package(url: "https://github.com/Carthage/Commandant.git", versions: Version(0, 11, 3)..<Version(0, 11, .max)),
+ 		.Package(url: "https://github.com/Quick/Nimble", majorVersion: 5),
+        .Package(url: "https://github.com/Quick/Quick", majorVersion: 0, minor: 10),
+    ]
+)
+
