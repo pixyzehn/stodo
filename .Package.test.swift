61965a649ee9edab62a4131973d415487bc4a281
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2017-02-27 08:28:48 +0900

    Update Nimble and Quick

--- .Package.test.swift
+++ .Package.test.swift
@@ -9,8 +9,8 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/Carthage/Commandant.git", versions: Version(0, 11, 3)..<Version(0, 11, .max)),
-        .Package(url: "https://github.com/Quick/Nimble", majorVersion: 5, minorVersion: 1),
-        .Package(url: "https://github.com/Quick/Quick", majorVersion: 1),
+        .Package(url: "https://github.com/Quick/Nimble", majorVersion: 6, minorVersion: 0),
+        .Package(url: "https://github.com/Quick/Quick", majorVersion: 1, minorVersion: 1),
     ]
 )
 
