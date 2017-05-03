0d42dee3d32b31e2ef96b2a8772cf42608711248
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2017-05-03 22:58:41 +0900

    Bump version to 0.1.2

--- .Package.test.swift
+++ .Package.test.swift
@@ -8,8 +8,8 @@ let package = Package(
               dependencies: [.Target(name: "StodoKit")]),
     ],
     dependencies: [
-        .Package(url: "https://github.com/Carthage/Commandant.git", versions: Version(0, 11, 3)..<Version(0, 11, .max)),
-        .Package(url: "https://github.com/Quick/Nimble", majorVersion: 6, minorVersion: 0),
+        .Package(url: "https://github.com/Carthage/Commandant.git", versions: Version(0, 12, 0)..<Version(0, 12, .max)),
+        .Package(url: "https://github.com/Quick/Nimble", majorVersion: 7, minorVersion: 0),
         .Package(url: "https://github.com/Quick/Quick", majorVersion: 1, minorVersion: 1),
     ]
 )
